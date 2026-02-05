35375d837bbdc74b6655635d61f70e3e10ad9250
Bartosz <bartosz.azymi@gmail.com>
2026-02-05 19:30:42 +0100

    tabs component small refactor

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -6,7 +6,6 @@ import * as TabsPrimitive from "@radix-ui/react-tabs";
 import { cn } from "@/lib/utils";
 
 const Tabs = TabsPrimitive.Root;
-//todo: Refactor
 const TabsList = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.List>,
   React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
@@ -29,7 +28,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50  data-[state=active]:bg-DarkGray data-[state=active]:text-foreground data-[state=active]:text-WhiteMain",
+      "rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all data-[state=active]:bg-DarkGray d data-[state=active]:text-WhiteMain",
       className,
     )}
     {...props}
@@ -43,10 +42,7 @@ const TabsContent = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <TabsPrimitive.Content
     ref={ref}
-    className={cn(
-      "mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
-      className,
-    )}
+    className={cn("mt-2 ", className)}
     {...props}
   />
 ));
